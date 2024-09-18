287e6fd4351124541e7b1448e424940d9b59ba37
sebastin-unknown <116891307+sebastin-unknown@users.noreply.github.com>
2024-09-18 09:09:56 -0700

    Add files via upload
    files added

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const path = require('path')
+const app = express();
+const db = require('./db');
+
+
+app.use("/assets", express.static('assets'))
+app.set("view engine","ejs")
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+  
+
+
+app.get('/', (req, res) => {
+  res.render('signup');
+});
+
+
+
+console.log("before")
+
+app.post('/signup', (req, res) => {
+   
+  const data = {
+    Name : req.body.Name ,
+    Email : req.body.Email ,
+    Password : req.body.Password,
+   
+
+  }
+  console.log(data.input)
+
+  db.query(`INSERT INTO sign_up (Name, Email ,Password) VALUES ("${data.Name}", "${data.Email}", "${data.Password}");`, (err, results) => {
+    if (err) {
+      console.error('Error inserting data:', err);
+      res.status(500).send('Error inserting data');
+      return;
+    }
+    res.render('login');
+  });
+
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
+   
+  
+});
+
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
+app.post('/login', (req, res) => {
+   
+  const data = {
+    Email : req.body.Email ,
+    Password : req.body.Password,
+   
+
+  }
+  console.log(data.input)
+
+  const check1 = db.query(`select * from sign_up where Email = "${data.Email}" and Password = "${data.Password}";`);
+  
+  if(check1 === null){
+    console.log("nodata found")
+  }else{
+    res.render("index")
+  }
+  
+
+  
+});
+
+
+
+
+
+
+
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+
+
+
+
+
+
+
+app.listen(3020, () => {
+  console.log('Server started on port 3020');
+});
